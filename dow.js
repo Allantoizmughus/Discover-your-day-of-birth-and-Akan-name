a5c5ae48456ba6875e173e026249153b2a923389
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-12 13:33:43 +0300

    Add function to js file

--- dow.js
+++ dow.js
@@ -1,14 +1,3 @@
-function dow() {
-    var CC = parseInt(document.querySelector("#century").value);
-    var YY = parseInt(document.querySelector("#year").value);
-    var MM = parseInt(document.querySelector("#month").value);
-    var DD = parseInt(document.querySelector("#day").value);
-    var op =document.querySelector("#operator").value;
-    var dayOfTheWeek;
-    if (op == "run"){
-        return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-
-    }
-    var dayOfTheWeeks=Math.abs((dayOfTheWeek.toFixed()));
-    document.querySelector("#results").innerHTML=dayOfTheWeeks;
-}
+function dow(){
+    let dateInput = document.getElementById("date").value;
+    let date = new Date(dateInput);
