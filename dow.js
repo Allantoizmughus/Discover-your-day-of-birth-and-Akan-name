99b7489cb1338485bee0df8014cbcc7b7669eecd
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-12 13:39:14 +0300

    Add function gender in js file

--- dow.js
+++ dow.js
@@ -11,3 +11,18 @@ function dow(){
             return;
         }
     }    
+
+    function gender() {
+        let gender;
+        if ((form.gender[0].checked == true) || (form.gender[1].checked == true)) {
+            if (form.gender[0].checked == true) {
+                gender = 0;
+            } else if (form.gender[1].checked == true) {
+                gender = 1;
+            }
+            return gender;
+        } else {
+            alert("Please select your gender!");
+            return;
+        }
+    }
