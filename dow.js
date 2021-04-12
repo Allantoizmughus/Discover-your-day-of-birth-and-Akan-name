86dcc543b7a1582926689a024442b740a2ddf957
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-12 13:35:31 +0300

    modify function dow

--- dow.js
+++ dow.js
@@ -1,3 +1,13 @@
 function dow(){
     let dateInput = document.getElementById("date").value;
     let date = new Date(dateInput);
+
+    if (!!date.valueOf()) {
+        let year = date.getFullYear().toString();
+        let dayOfWeek = date.getDay();
+            return dayOfWeek;
+        } else {
+            alert("Please select your birthday.");
+            return;
+        }
+    }    
