282d837fec01d2bc6e028bf2180c8bb4556fa8b4
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-12 11:46:33 +0300

    Modify the js file

--- dow.js
+++ dow.js
@@ -1,10 +1,10 @@
-function calc() {
+function dow() {
     var CC = parseInt(document.querySelector("#century").value);
     var YY = parseInt(document.querySelector("#year").value);
     var MM = parseInt(document.querySelector("#month").value);
     var DD = parseInt(document.querySelector("#day").value);
     var op =document.querySelector("#operator").value;
-    var dow;
+    var dayOfTheWeek;
     if (op == "run"){
         return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
 
