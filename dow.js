8d4bf7182b60e8754af6327e5fe9533bde97270b
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-12 11:32:41 +0300

    Add function in js

---
+++ dow.js
@@ -0,0 +1,12 @@
+function calc() {
+    var CC = parseInt(document.querySelector("#century").value);
+    var YY = parseInt(document.querySelector("#year").value);
+    var MM = parseInt(document.querySelector("#month").value);
+    var DD = parseInt(document.querySelector("#day").value);
+    var op =document.querySelector("#operator").value;
+    var dow;
+    if (op == "run"){
+        return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+
+    }
+}
